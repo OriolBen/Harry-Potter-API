97e60f687eda29f1a7b3887b5a4832923a44377c
OriolBen <oriol.ben.laravel@gmail.com>
2020-03-11 02:09:52 +0100

    2 files modified, 152 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { ApiServiceService } from './api-service.service';
 import { HomeComponent } from './components/home/home.component';
@@ -14,10 +13,20 @@ import { HousesComponent } from './components/houses/houses.component';
 import { HouseComponent } from './components/house/house.component';
 import { SpellsComponent } from './components/spells/spells.component';
 
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule, FontAwesomeModule, AppRoutingModule ],
+  imports:      [ BrowserModule, FormsModule, HttpClientModule, AppRoutingModule, FontAwesomeModule ],
   declarations: [ AppComponent, HomeComponent, CharactersComponent, HousesComponent, SpellsComponent, CharacterComponent, HouseComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ApiServiceService]
 })
-export class AppModule { }
+
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fab, far, fas);
+  }
+}
