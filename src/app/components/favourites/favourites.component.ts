4cc14bcb005d6754beeddb274227d8408e9e0c78
OriolBen <oriol.ben.laravel@gmail.com>
2020-03-18 18:43:33 +0100

    2 files modified

--- src/app/components/favourites/favourites.component.ts
+++ src/app/components/favourites/favourites.component.ts
@@ -9,17 +9,51 @@ import { Favourite, DataService } from '../../data.service'
 })
 
 export class FavouritesComponent implements OnInit {
-  houses : object
+  house : object
+  houseCharacters : object = {}
   characters : Array<any>
   spells : Array<any>
   local : Favourite
-  empty : boolean = false
+  empty : boolean
+  emptyHouse : boolean
+  emptyCharacters : boolean
+  emptySpells : boolean
 
   constructor(private api : ApiService, private storage : DataService) {}
 
   ngOnInit() {
     this.local = this.storage.getFavourite()
-    console.log(this.local)
+    this.emptyHouse = this.local.house == "" ? true : false
+    this.emptyCharacters = this.local.characters.length == 0 ? true : false
+    this.emptySpells = this.local.spells.length == 0 ? true : false
+    if (!this.emptyHouse) this.getHouse()
+    this.empty = this.emptyHouse && this.emptyCharacters && this.emptySpells ? true : false
   }
 
+  getHouse() : void {
+    this.api.getHouse(this.local.house).subscribe((data : object) => {
+      this.house = data[0]
+      this.getCharactersNames()
+    }) 
+  }
+
+  getCharactersNames() : void {
+    this.api.getAllCharacters().subscribe((data : Array<any>) => {
+      data.forEach((character) => {
+        if (character.house == this.house["name"]) this.houseCharacters[character._id] = character.name
+      })
+    })
+  }
+
+  removeHouse(id : string) : void {
+    this.local.house = this.storage.removeFavourite("house", id).house
+  }
+
+  checkHouse(id : string) : boolean {
+    return this.local.house == id
+  }
+
+  checkHouseCharacters(id : string) : boolean {
+    return id in this.houseCharacters
+  }
 }
