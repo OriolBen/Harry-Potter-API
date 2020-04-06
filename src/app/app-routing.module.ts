00d91a53a3f6134c8790e1a298982554d030748e
OriolBen <oriol.ben.laravel@gmail.com>
2020-04-06 19:03:49 +0200

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ import { HousesComponent } from './components/houses/houses.component'
 import { HouseComponent } from './components/house/house.component'
 import { SpellsComponent } from './components/spells/spells.component'
 import { FavouritesComponent } from './components/favourites/favourites.component'
+import { LoginComponent } from './components/login/login.component'
+import { RegisterComponent } from './components/register/register.component'
+import { ResetComponent } from './components/reset/reset.component'
+import { AccountComponent } from './components/account/account.component'
 
 const routes: Routes = [
   {
@@ -37,6 +41,22 @@ const routes: Routes = [
     path: 'favourites',
     component: FavouritesComponent
   },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'reset',
+    component: ResetComponent
+  },
+  {
+    path: 'account',
+    component: AccountComponent
+  }
 ]
 
 @NgModule({
@@ -44,4 +64,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 
-export class AppRoutingModule {}
+export class AppRoutingModule { }
