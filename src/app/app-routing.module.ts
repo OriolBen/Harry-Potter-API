2bc98249b98fa7b3960bbc5e310083012945e7ab
OriolBen <oriol.ben.laravel@gmail.com>
2020-04-06 18:43:55 +0200

    2 files modified, 28 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { CharactersComponent } from './components/characters/characters.component';
-import { CharacterComponent } from './components/character/character.component';
-import { HousesComponent } from './components/houses/houses.component';
-import { HouseComponent } from './components/house/house.component';
-import { SpellsComponent } from './components/spells/spells.component';
-import { FavouritesComponent } from './components/favourites/favourites.component';
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
+import { HomeComponent } from './components/home/home.component'
+import { CharactersComponent } from './components/characters/characters.component'
+import { CharacterComponent } from './components/character/character.component'
+import { HousesComponent } from './components/houses/houses.component'
+import { HouseComponent } from './components/house/house.component'
+import { SpellsComponent } from './components/spells/spells.component'
+import { FavouritesComponent } from './components/favourites/favourites.component'
 
 const routes: Routes = [
   {
@@ -37,11 +37,11 @@ const routes: Routes = [
     path: 'favourites',
     component: FavouritesComponent
   },
-];
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,22 @@
+// Modules
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { FormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http'
-
-import { AppComponent } from './app.component'
+import { RouterModule } from '@angular/router'
 import { AppRoutingModule } from './app-routing.module'
-import { ApiService } from './api.service'
+
+// Firebase
+import { AngularFireModule } from '@angular/fire'
+import { AngularFireAuth } from '@angular/fire/auth'
+
+// Services
+import { ApiService } from './services/api.service'
+import { DataService } from './services/data.service'
+import { AuthenticationService } from './services/authentication.service'
+
+// Components
+import { AppComponent } from './app.component'
 import { HomeComponent } from './components/home/home.component'
 import { CharactersComponent } from './components/characters/characters.component'
 import { CharacterComponent } from './components/character/character.component'
@@ -14,13 +25,54 @@ import { HouseComponent } from './components/house/house.component'
 import { SpellsComponent } from './components/spells/spells.component'
 import { FooterComponent } from './components/footer/footer.component'
 import { FavouritesComponent } from './components/favourites/favourites.component'
-import { DataService } from './data.service'
+import { LoginComponent } from './components/login/login.component'
+import { RegisterComponent } from './components/register/register.component'
+import { BarComponent } from './components/bar/bar.component'
+import { ResetComponent } from './components/reset/reset.component'
+import { AccountComponent } from './components/account/account.component'
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule, AppRoutingModule ],
-  declarations: [ AppComponent, HomeComponent, CharactersComponent, HousesComponent, SpellsComponent, CharacterComponent, HouseComponent, FooterComponent, FavouritesComponent ],
-  bootstrap:    [ AppComponent ],
-  providers: [ApiService, DataService]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyDcp3ojggM75TPgdLz6zUAaZrlWLIetEiA",
+      authDomain: "harry-potter-api-e764c.firebaseapp.com",
+      databaseURL: "https://harry-potter-api-e764c.firebaseio.com",
+      projectId: "harry-potter-api-e764c",
+      storageBucket: "harry-potter-api-e764c.appspot.com",
+      messagingSenderId: "375890053637",
+      appId: "1:375890053637:web:1f83020707079f55e2aa6c"
+    })
+  ],
+  declarations: [ 
+    AppComponent,
+    HomeComponent,
+    CharactersComponent,
+    HousesComponent,
+    SpellsComponent,
+    CharacterComponent,
+    HouseComponent,
+    FooterComponent,
+    FavouritesComponent,
+    LoginComponent,
+    RegisterComponent,
+    BarComponent,
+    ResetComponent,
+    AccountComponent
+  ],
+  bootstrap: [ 
+    AppComponent
+  ],
+  providers: [
+    ApiService,
+    DataService,
+    AuthenticationService,
+    AngularFireAuth
+  ]
 })
 
 export class AppModule { }
